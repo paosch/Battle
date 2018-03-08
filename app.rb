98b430bd240297f1958b8155d4980e2399ddf830
paosch <paola_schiaffino@yahoo.co.uk>
2018-03-08 16:49:13 +0000

    Added players

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   # "Testing infrastructure working!"
+  enable :sessions
   get '/' do
     erb :index
   end
@@ -9,11 +10,24 @@ class Battle < Sinatra::Base
   post '/names' do # index file sends us to this route
     # in order to pass the variables into the view(=erb file),
     # we extract them from the params in the post '/names' action
-    @player_1_name = params[:player_1_name] # make whatever the player enters equal to player_1_name
-    @player_2_name = params[:player_2_name]
+    #@player_1_name = params[:player_1_name] # make whatever the player enters equal to player_1_name
+    #@player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
-
+  get '/attack' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :attack
+  end
+  # to link the result of the get '/play' route with the last route: use a link in play.erb
+  
   run! if app_file == $0
 end
 
